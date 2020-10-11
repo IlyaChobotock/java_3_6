24cb35224075f3e8fbaeceae83e4514e717ed23c
Chobotock <ilya_ageev@bk.ru>
2020-10-11 13:14:27 +0300

    lesson_3_6

---
+++ src/test/java/TestAllArr.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestArr.class,
+        TestParam1Arr.class,
+        TestParam2Arr.class})
+public class TestAllArr {
+}
